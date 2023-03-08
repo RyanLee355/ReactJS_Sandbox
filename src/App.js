2ef88a4ba11dd926d8d329f591df732cf7260eb2
Ryan_Lee <psytl3@nottingham.ac.uk>
2023-03-08 22:55:55 +0000

    Completed Nav + Landing Hero

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Navbar from "./components/Navbar"
+import Landing from "./components/Landing"
+import Gallery from "./components/trial/Gallery"
+import Banners from "./components/trial/Banners"
+import WebDisplay from "./components/trial/WebDisplay"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// https://colorhunt.co/palette/ecf2ff3e54ac655dbbbface2
+
+export default function App() {
+    return (
+        <div>
+            <Navbar />
+            <Landing />
+            {/* <WebDisplay /> */}
+            {/* <Gallery /> */}
+            {/* <Banners /> */}
+        </div>
+    )
 }
-
-export default App;
